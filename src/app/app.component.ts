67c8eb049663483e0a6d3772330fd992617914c7
abdulrahman alaa <abdulrahman@parent.eu>
2018-04-25 10:11:54 +0200

    Initial project structure

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'tj-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'tj';
+}
